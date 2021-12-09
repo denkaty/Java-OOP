9cbdd964901dc9d919e7848e338e2acfbc86d173
denkaty <82085828+denkaty@users.noreply.github.com>
2021-12-09 22:22:56 +0200

    Add file

---
+++ 07.Interfaces and Abstraction - Lab/01.CarShop/Car.java
@@ -0,0 +1,16 @@
+package CarShop_01;
+
+public interface Car {
+    int TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+
+
+
+
+
+
+}
